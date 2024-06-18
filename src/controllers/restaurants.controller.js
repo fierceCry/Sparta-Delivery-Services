e54ace52c06d9b995e44c26a41c466b605234410
jeonsuwon <swjeon0330@gmail.com>
2024-06-18 15:08:10 +0900

    commentFix

--- src/controllers/restaurants.controller.js
+++ src/controllers/restaurants.controller.js
@@ -1,11 +1,12 @@
 import { RestaurantsService } from '../services/restaurants.services.js';
+import { HTTP_STATUS } from '../constants/http-status.constant.js';
 
 export class RestaurantsController {
   restaurantsService = new RestaurantsService();
   getRestaurants = async (req, res) => {
     const restaurants = req.user;
 
-    return res.status(200).json({
+    return res.status(HTTP_STATUS.OK).json({
       message: '정상적으로 정보조회가 완료되었습니다.',
       data: restaurants,
     });
@@ -28,7 +29,7 @@ export class RestaurantsController {
       restaurantNumber
     );
 
-    return res.status(201).json({
+    return res.status(HTTP_STATUS.CREATED).json({
       message: '정상적으로 정보수정이 완료되었습니다.',
       data: restaurants,
     });
--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,9 +1,11 @@
+import { HTTP_STATUS } from '../constants/http-status.constant.js';
+
 export class UsersController {
   getUser = async (req, res) => {
     const user = req.user;
 
     res
-      .status(200)
+      .status(HTTP_STATUS.OK)
       .json({ message: '정상적으로 정보조회가 완료되었습니다.', data: user });
     return;
   };
